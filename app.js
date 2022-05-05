92b4cced86f51924a94618c9ec9235dfda3df0f9
elmi-elmi <shahrokhelmi@gmail.com>
2022-05-05 12:00:03 +0430

    l3-end

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-const fs = require('fs');
+const getNotes = require('./notes.js');
 
-fs.writeFileSync('notes.txt', 'This is my first text file');
-fs.appendFileSync('notes.txt', '\n');
-fs.appendFileSync('notes.txt', 'This is my second text file');
-
-// add a break line after each file
+console.log(getNotes());
 
 
