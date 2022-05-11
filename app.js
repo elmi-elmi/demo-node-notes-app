2a6b14d03db389b403bf9c7a004d139c787a4f1a
elmi-elmi <shahrokhelmi@gmail.com>
2022-05-11 16:29:46 +0430

    l3-start

--- app.js
+++ app.js
@@ -1,5 +1,42 @@
 const getNotes = require('./notes.js');
+const chalk = require('chalk');
 
-console.log(getNotes());
 
+const yargs = require('yargs');
+
+yargs.version('1.1.0');
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding note');
+    }
+})
+
+yargs.command({
+    command:'remove',
+    describe:'Remove a note',
+    handler:function(){
+        console.log('Removing note');
+    }
+})
+
+yargs.command({
+    command:'list',
+    describe:'List all notes',
+    handler:function(){
+        console.log('Listing all notes');
+    }
+}
+)
+yargs.command({
+    command:'read',
+    describe:'Read a note',
+    handler:function(){
+        console.log('Reading a note');
+    }
+})
+
+console.log(yargs.argv);
 
